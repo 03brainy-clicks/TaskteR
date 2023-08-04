33499f23e1a1c2d750dd87125e47802c26355641
Chandrakant Kushwah <chandrakant.kushwah2019@sait.ac.in>
2023-08-04 16:07:59 +0530

    first commit

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 import Filter from "@/components/Filter";
 import Tasklist from "@/components/task/TaskList";
 import GetDate from "@/utils/getdate";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { faMinus, faPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 import React from "react";
@@ -14,13 +14,12 @@ const Home = () => {
           <h3 className="text-xl font-bold">Today&#39;s Task</h3>
           <p className="text-gray-500 ">{GetDate()}</p>
         </div>
-        <div>
-          <Link href={"/create"}>
-            <button className="text-sm transition-all duration-300 font-medium py-2 px-5 rounded text-white bg-black hover:bg-gray-800 active:bg-gray-700">
-              <FontAwesomeIcon icon={faPlus} className="mr-1" /> New Task
-            </button>
-          </Link>
-        </div>
+
+        <Link href={"/create"}>
+          <button className="text-sm transition-all duration-300 font-medium py-2 px-5 rounded text-white bg-black hover:bg-gray-800 active:bg-gray-700">
+            <FontAwesomeIcon icon={faPlus} className="mr-1" /> New Task
+          </button>
+        </Link>
       </div>
       <Filter />
       <Tasklist />
